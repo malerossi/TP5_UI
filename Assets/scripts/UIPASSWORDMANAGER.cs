a161a6bfd34737c2bc409f1bdba957536f98bb6d
malerossi <malena.agostina.rossi@gmail.com>
2026-04-13 14:30:12 -0300

    cambios 13/04

---
+++ Assets/scripts/UIPASSWORDMANAGER.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIPASSWORDMANAGER : MonoBehaviour
+{
+    public GameObject txtResultadoContra;
+    public TextMeshProUGUI txtResultadoContraTexto;
+    public TMP_InputField input_Password;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtResultadoContra.SetActive(false);
+    }
+
+    public void ValidatePassword(){
+        txtResultadoContra.SetActive(true);
+        txtResultadoContraTexto.text = input_Password.text;
+    }
+}
